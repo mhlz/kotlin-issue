8f55da5622a5b3576730bb1d989fd45551f73164
Mischa Holz <me@mhlz.de>
2018-09-14 12:41:07 +0200

    Demonstrate ktlint issue

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,5 @@
-
-annotation class NoArg
-
-sealed class Test {
-
-    abstract val test: String
-
-    @NoArg
-    data class Test1(
-            override val test: String
-    ) : Test()
-
-    @NoArg
-    data class Test2(
-            override val test: String
-    ) : Test()
-}
+package de.mhlz.test
 
 fun main(args: Array<String>) {
-
-    val problemConstructor = Test::class.java.getDeclaredConstructor()
-    problemConstructor.isAccessible = true
-
-    val javaConstructor = Test.Test1::class.java.getConstructor()
-            ?: error("Did not find constructor")
-    javaConstructor.isAccessible = true
-
-    val instance = javaConstructor.newInstance()
-
+    println("hello!")
 }
---
+++ src/main/kotlin/model/TestClass.kt
@@ -0,0 +1,5 @@
+package de.mhlz.test.model
+
+data class TestClass(
+    val test: String
+)
